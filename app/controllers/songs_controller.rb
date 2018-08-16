19b5642510051638faf31791d39a76e4d00bb5b1
Vishal Sharma <vsh1819@gmail.com>
2018-08-16 07:54:40 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,13 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def upload
+   CSV.foreach(params["file"].path, headers: true) do |song|
+     Song.create(title: song[0], artist_name: song[1])
+   end
+   redirect_to songs_path
+ end
+
   def create
     @song = Song.new(song_params)
 
@@ -54,4 +61,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
